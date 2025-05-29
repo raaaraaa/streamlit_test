3ca511265e688d94ef67e2aff906bf93f7093839
raaaraaa <srhhzqh30@gmail.com>
2025-05-29 15:16:28 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 # Set the app title 
-st.title('My First Streamlit App !!') 
+st.title('Blupp Blupp raraaa !!') 
 
 # Add a welcome message 
 st.write('Welcome to my Streamlit app!') 
